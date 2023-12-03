4566509bc012c1987ffffe9a6cc7a7da6ce50ee4
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-03 21:45:40 +0200

    fix login

--- backend/php/connection.php
+++ backend/php/connection.php
@@ -6,7 +6,7 @@ header('Access-Control-Max-Age: 86400');
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "hospitaldb";
+$db_name = "hospital_system";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 ?>
