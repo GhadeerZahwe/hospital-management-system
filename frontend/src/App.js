c8117f27d717062054761f0158cd2d6444a8be54
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-04 19:41:55 +0200

    install axios and react route and fix react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/Login';
+import RegisterPage from './components/Register';
+import AdminPortalPage from './pages/AdminPortalPage';
+import PatientPortalPage from './pages/PatientPortalPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/admin-portal" component={AdminPortalPage} />
+      <Route path="/patient-portal" component={PatientPortalPage} />
+    </Router>
   );
 }
 
