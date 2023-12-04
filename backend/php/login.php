dba36cd81427143d4187f425e01dc00c3f530b06
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-04 14:12:20 +0200

    fix login + add error handling

--- backend/php/login.php
+++ backend/php/login.php
@@ -1,33 +1,44 @@
 <?php
 include('connection.php');
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$response = array(); // Initialize the response array
 
-$query = $mysqli->prepare('select users.id,users.first_name,users.last_name,users.email,users.password,user_types.name from users inner join user_types on users.usertype_id = user_types.id where email=?');
-$query->bind_param('s', $email);
-$query->execute();
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$query->store_result();
-$num_rows = $query->num_rows();
-$query->bind_result($id, $first_name, $last_name, $email, $hashed_password,$usertype);
-$query->fetch();
-$response = [];
-if ($num_rows == 0) {
-    $response['response'] = "user not found";
-} else {
-    
-    if (password_verify($password, $hashed_password)) {
-        $response['response'] = "logged in";
-        $response['user_id'] = $id;
-        $response['first_name'] = $first_name;
-        $response['last_name'] = $last_name;
-        $response['email'] = $email;
-        $response['usertype'] = $usertype;
-        
+    $query = $mysqli->prepare('SELECT users.id, users.first_name, users.last_name, users.email, users.password, user_types.name 
+                               FROM users 
+                               INNER JOIN user_types ON users.usertype_id = user_types.id 
+                               WHERE email = ?');
+    $query->bind_param('s', $email);
+    $query->execute();
+
+    $query->store_result();
+    $num_rows = $query->num_rows();
+    $query->bind_result($id, $first_name, $last_name, $email, $hashed_password, $usertype);
+    $query->fetch();
+
+    if ($num_rows == 0) {
+        $response['status'] = "failed";
+        $response['message'] = "User not found";
     } else {
-        $response["response"] = "Incorrect password";
+        if (password_verify($password, $hashed_password)) {
+            $response['status'] = "success";
+            $response['message'] = "Logged in";
+            $response['user_id'] = $id;
+            $response['first_name'] = $first_name;
+            $response['last_name'] = $last_name;
+            $response['email'] = $email;
+            $response['usertype'] = $usertype;
+        } else {
+            $response['status'] = "failed";
+            $response['message'] = "Incorrect password";
+        }
     }
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "Incomplete POST data";
 }
 
 echo json_encode($response);
