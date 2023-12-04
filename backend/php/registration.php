17dc5772ea993ffca484eb26e7632f932c1d7e2e
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-04 14:06:50 +0200

    fix register + add error handling

--- backend/php/registration.php
+++ backend/php/registration.php
@@ -2,36 +2,54 @@
 
 include('connection.php');
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$user_type= $_POST['user_type'];
+$response = array(); // Initialize the response array
 
-$check_email =  $mysqli->prepare('select email from users where email=?');
-$check_email->bind_param('s', $email);
-$check_email->execute();
-$check_email->store_result();
-$email_exists = $check_email->num_rows();
+if (
+    isset($_POST['first_name']) &&
+    isset($_POST['last_name']) &&
+    isset($_POST['email']) &&
+    isset($_POST['password']) &&
+    isset($_POST['user_type'])
+) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $user_type = $_POST['user_type'];
 
-$query =  $mysqli->prepare('select id from user_types where name=?');
-$query->bind_param('s', $user_type);
-$query->execute();
-$array=$query->get_result();
-$user_type_id= $array->fetch_assoc();
+    $check_email =  $mysqli->prepare('SELECT email FROM users WHERE email = ?');
+    $check_email->bind_param('s', $email);
+    $check_email->execute();
+    $check_email->store_result();
+    $email_exists = $check_email->num_rows();
 
-$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    if ($email_exists > 0) {
+        $response['status'] = "failed";
+        $response['message'] = "Email already exists";
+    } else {
+        $query =  $mysqli->prepare('SELECT id FROM user_types WHERE name = ?');
+        $query->bind_param('s', $user_type);
+        $query->execute();
+        $array = $query->get_result();
+        $user_type_id = $array->fetch_assoc();
 
-if ($email_exists > 0) {
-    $response['status'] = "failed";
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $insert_query =  $mysqli->prepare('INSERT INTO users(first_name, last_name, email, password, usertype_id) VALUES (?, ?, ?, ?, ?)');
+        $insert_query->bind_param('ssssi', $first_name, $last_name, $email, $hashed_password, $user_type_id['id']);
+        
+        if ($insert_query->execute()) {
+            $response['status'] = "success";
+            $response['message'] = "User registered successfully";
+        } else {
+            $response['status'] = "failed";
+            $response['message'] = "Registration failed. Please try again later.";
+        }
+    }
 } else {
-    $query =  $mysqli->prepare('insert into users(first_name,last_name,email,password,usertype_id) values(?,?,?,?,?)');
-    $query->bind_param('ssssi', $first_name, $last_name, $email, $hashed_password,$user_type_id['id']);
-    $query->execute();
-    $response['status'] = "success";
+    $response['status'] = "failed";
+    $response['message'] = "Incomplete POST data";
 }
 
 echo json_encode($response);
-
-
 ?>
