cf7f3e0a6e21eb7315faace59b64c2997b280245
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-04 14:25:20 +0200

    fix assign users + add error handling

--- backend/php/assign_users.php
+++ backend/php/assign_users.php
@@ -2,15 +2,33 @@
 
 include('connection.php');
 
-$user_id= $_POST['user_id'];
-$hospital_id = $_POST['hospital_id'];
+$response = array(); // Initialize the response array
 
-$query = $mysqli->prepare('insert into hospital_users(hospital_id,user_id) values(?,?)');
-$query->bind_param('ii', $hospital_id,$user_id);
-$query->execute();
-$response['status'] = "success";
+if (isset($_POST['user_id'], $_POST['hospital_id'])) {
+    $user_id = $_POST['user_id'];
+    $hospital_id = $_POST['hospital_id'];
+
+    $query = $mysqli->prepare('INSERT INTO hospital_users (hospital_id, user_id) VALUES (?, ?)');
+    if ($query) {
+        $query->bind_param('ii', $hospital_id, $user_id);
+        $result = $query->execute();
+
+        if ($result) {
+            $response['status'] = "success";
+        } else {
+            $response['status'] = "failed";
+            $response['message'] = "Error executing the query: " . $mysqli->error;
+        }
+
+        $query->close();
+    } else {
+        $response['status'] = "failed";
+        $response['message'] = "Error preparing the SQL statement: " . $mysqli->error;
+    }
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "Incomplete POST data";
+}
 
 echo json_encode($response);
-
-
 ?>
