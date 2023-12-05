cb01f0111ca34dbead6fb09652ce883390f5c1f1
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-05 13:37:41 +0200

    frontend

--- backend/php/connection.php
+++ backend/php/connection.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:GET,POST,OPTIONS');
+header('Access-Control-Allow-Headers:*');
 
 $host = "localhost";
 $db_user = "root";
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      {/* <BrowserRouter>
+        <Routes>
+           <Route index element={<LogIn />} /> 
+        
+        </Routes>
+      </BrowserRouter>  */}
+        <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
